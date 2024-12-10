42bcfda4fba28bad6c081291707683d4be227f97
Julianore2004 <julianore79@gmail.com>
2024-12-10 17:25:37 -0500

    1231

--- login.php
+++ login.php
@@ -36,6 +36,12 @@ if ($login->isUserLoggedIn() == true) {
   <title>Simple Stock | Login</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<style>
+    body {
+      background: url('img/login-bg.jpg') no-repeat center center fixed;
+      background-size: cover;
+    }
+  </style>
   <!-- CSS  -->
    <link href="css/login.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>

fa1b2098ed2bdd05798270b631cabe7e6360dafe
Julianore2004 <julianore79@gmail.com>
2024-12-10 17:00:46 -0500

    update

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', 'guardian.tale3');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'simple_stock');//Nombre de la base de datos
+
+/*Datos de conexion a la base de datos*/
+/* define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'importec_inventario_xtreme');//Usuario de tu base de datos
+define('DB_PASS', 'inventario_xtreme');//Contrase単a del usuario de la base de datos
+define('DB_NAME', 'importec_inventario_xtreme');//Nombre de la base de datos
+ */
+?>
